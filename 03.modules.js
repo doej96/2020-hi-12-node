d50f4e96d224c79993817636992a516db968ae2a
doej96 <doej96@naver.com>
2021-01-18 15:17:58 +0900

    ..

--- 03.modules.js
+++ 03.modules.js
@@ -1,4 +1,9 @@
-//npm init-y : node파일로 만듦
+//npm init-y : node파일로 만듦 - package.json 만들어짐
 //npm i moment..
+
 //git에 올릴 때는 node_modules 올릴 필요X, npm에 있기 때문에 내가 관리할 파일 아님
 //.gitignore파일 만들어서 git에 올리지 않을 파일 적음 (/node_modules)
+/* 
+git에 올라와있는 파일 다시 깔 때는 node_modules파일 없지만
+package jason에 다 기록돼 있기 때문에 npm i하면 깔림
+ */
