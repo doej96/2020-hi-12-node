c17d304ea3c5fd938b59b37e6c146e5151be73eb
doej96@naver.com <doej96@naver.com>
2021-01-20 17:29:46 +0900

    ...

---
+++ CRUD.sql
@@ -0,0 +1,17 @@
+-- CREATE(INSERT) READ(SELECT) UPDATE(UPDATE) DELETE(DELETE)
+
+-- INSERT INTO city SET	
+-- name='Incheon',
+-- country='Korea',
+-- summary='한국의 제3의 도시',
+-- lat=37.27,
+-- lon=126.42,
+-- population=2923000,S
+-- sdate='1400-01-01';
+
+-- DELETE FROM	city WHERE id=4;
+
+-- UPDATE city SET
+-- summary='한국의 제2의 도시-도로가 좁다. 동백섬도 좋다.',
+-- population=2923012
+-- WHERE	id=2;
